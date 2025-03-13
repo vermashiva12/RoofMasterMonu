23927d307633a533e458a8336ce46aff0bef0d97
vermashiva12 <115514079+vermashiva12@users.noreply.github.com>
2025-03-12 20:12:39 -0400

    Update Logo.tsx

--- client/src/components/Logo.tsx
+++ client/src/components/Logo.tsx
@@ -1,5 +1,5 @@
 import { cn } from "@/lib/utils";
-
+import {RoofMaster} from "../static/RoofMasters-2.svg";
 
 interface LogoProps {
   className?: string;
@@ -10,8 +10,8 @@ export default function Logo({ className }: LogoProps) {
     <div className={cn("flex items-center gap-2", className)}>
 
       <img 
-        src="../static/RoofMasters-2.svg" 
-        alt="Roof Masters Logo" 
+        src={RoofMaster}
+        alt="Roof Masters" 
         className="w-10 h-10"
       />
       <span className="text-2xl font-bold text-primary">
